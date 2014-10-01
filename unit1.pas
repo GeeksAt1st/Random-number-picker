a6b8b08a26e0fb2fa1b19bf6e33280f768e52a5f
wxk6b1203 <you@exam>
2014-10-01 16:41:16 +0800

    Building

--- unit1.pas
+++ unit1.pas
@@ -12,10 +12,17 @@ type
   { TfrmMain }
 
   TfrmMain = class(TForm)
+    btnOK: TButton;
+    edtInput: TEdit;
+    Label1: TLabel;
+    lblWarning: TLabel;
     lblNameEN: TLabel;
     lblV: TLabel;
     lblAboutTitle: TLabel;
     lblMain: TLabel;
+    procedure edtInputChange(Sender: TObject);
+    procedure edtInputClick(Sender: TObject);
+    procedure edtInputExit(Sender: TObject);
   private
     { private declarations }
   public
@@ -28,6 +35,48 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.edtInputClick(Sender: TObject);
+begin
+  edtInput.Font.Size:=25;
+  If edtInput.Text='Input the count.' then
+  edtInput.Text:='';
+  end;
+
+procedure TfrmMain.edtInputChange(Sender: TObject);
+var a:string; g,s:integer;c:char;
+begin
+  edtInput.font.Size:=25;
+  s:=length(edtInput.Text);
+  a:=edtInput.Text;
+  for g:=1 to s do
+  begin
+  c:=a[g];
+  if (ord(c)>57) or (ord(c)<48) then
+  begin
+  lblwarning.Caption:='您输入了除数字以外的其他字符';
+  lblwarning.Visible:=true;
+  break;
+  end
+  else
+  lblwarning.Visible:=false;
+  if edtInput.Text='Input the count' then
+  lblwarning.Visible:=false ;
+  end;
+  end;
+
+
+
+
+procedure TfrmMain.edtInputExit(Sender: TObject);
+begin
+
+  if edtInput.Text='' then
+   edtInput.Font.size:=18;
+  end;
+
 
 end.
 
